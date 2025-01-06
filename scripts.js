6ac17941292501de8ccf225417370f7b56742638
fertcore <devlovatofertcore@gmail.com>
2025-01-06 18:43:55 +0000

    botchat

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+<script type="text/javascript">
+(function(d, t) {
+    var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+    v.onload = function() {
+        window.voiceflow.chat.load({
+            verify: { projectID: '668ea792f5272a4816372bea' },
+            url: 'https://general-runtime.voiceflow.com',
+            versionID: 'production'
+        });
+    }
+    v.src = "https://cdn.voiceflow.com/widget/bundle.mjs"; 
+    v.type = "text/javascript"; 
+    s.parentNode.insertBefore(v, s);
+})(document, 'script');
+</script>
